fe603251f0cc899b1286c6515c4e023686d1455a
Marina Kuchmasova <kuchmasova.m@gmail.com>
2023-01-21 22:29:06 +0300

    variable correction + code formatting

---
+++ src/BonusMilesService.java
@@ -0,0 +1,10 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+
+        int formula = 20;
+
+        int bonus = cost /formula;
+
+        return bonus;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int amount = 100; //стоимость билета
-        int bonusСalculation = 20; //количество рублей для одной мили
-        int bonus = amount / bonusСalculation; //количество бонусных миль
-        System.out.println("Количество начисленных миль за купленный билет: " + bonus);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
